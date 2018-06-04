9a96ebe590a0aa60ce14e92fb778f28759f1abaa
sabertazimi <sabertazimi@gmail.com>
2018-06-04 20:41:53 +0800

    refactor(mpm.js): create mpm.js module, move functions to it

--- src/index.js
+++ src/index.js
@@ -1,54 +1,7 @@
-const fs = require('fs-extra');
-const fetch = require('node-fetch');
-const semver = require('semver');
-const REGISTRY_URL = 'https://registry.npm.taobao.org';
+const Mpm = require('./mpm.js');
+const mpm = new Mpm();
 
-async function getPinnedReference({ name, reference }) {
-  if (semver.validRange(reference) && !semver.valid(reference)) {
-    const response = await fetch(`${REGISTRY_URL}/${name}`);
-
-    if (!response.ok) {
-        throw new Error(`Couldn't fetch package "${name}"`);
-    }
-
-    const info = await response.json();
-    const versions = Object.keys(info.versions);
-    const maxSatisfying = semver.maxSatisfying(versions, reference);
-
-    if (maxSatisfying === null) {
-      throw new Error(
-        `Couldn't find a version matching "${reference}" for package  "${name}"`
-      );
-    }
-
-    reference = maxSatisfying;
-  }
-
-  return { name, reference };
-}
-
-async function fetchPackage({ name, reference }){
-    if ([`/`, `./`, `../`].some(prefix => reference.startsWith(prefix))) {
-        return await fs.readFile(reference);
-    }
-
-    if (semver.valid(reference)) {
-        return await fetchPackage({
-            name,
-            reference: `${REGISTRY_URL}/${name}/-/${name}-${reference}.tgz`
-        });
-    }
-
-    const response =  await fetch(reference);
-
-    if (!response.ok) {
-        throw new Error(`Couldn't fetch package "${reference}"`);
-    }
-
-    return await response.buffer();
-}
-
-fetchPackage({
+mpm.fetchPackage({
     name: 'react',
     reference: '16.4.0'
 }).then(buffer => console.log(buffer));
