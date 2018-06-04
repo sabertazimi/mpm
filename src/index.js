219e35e52c476dabcacae08440789ed14a476516
sabertazimi <sabertazimi@gmail.com>
2018-06-04 20:10:27 +0800

    add(pinned-version): impl getPinnedReference function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,30 @@ const fetch = require('node-fetch');
 const semver = require('semver');
 const REGISTRY_URL = 'https://registry.npm.taobao.org';
 
+async function getPinnedReference({ name, reference }) {
+  if (semver.validRange(reference) && !semver.valid(reference)) {
+    const response = await fetch(`${REGISTRY_URL}/${name}`);
+
+    if (!response.ok) {
+        throw new Error(`Couldn't fetch package "${name}"`);
+    }
+
+    const info = await response.json();
+    const versions = Object.keys(info.versions);
+    const maxSatisfying = semver.maxSatisfying(versions, reference);
+
+    if (maxSatisfying === null) {
+      throw new Error(
+        `Couldn't find a version matching "${reference}" for package  "${name}"`
+      );
+    }
+
+    reference = maxSatisfying;
+  }
+
+  return { name, reference };
+}
+
 async function fetchPackage({ name, reference }){
     if ([`/`, `./`, `../`].some(prefix => reference.startsWith(prefix))) {
         return await fs.readFile(reference);
@@ -15,7 +39,7 @@ async function fetchPackage({ name, reference }){
         });
     }
 
-    let response =  await fetch(reference);
+    const response =  await fetch(reference);
 
     if (!response.ok) {
         throw new Error(`Couldn't fetch package "${reference}"`);
